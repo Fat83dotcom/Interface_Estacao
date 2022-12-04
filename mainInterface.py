af82de3f85ce27271d1c2481978a760f3525a7b7
fat83dotcom <fernando.quimic@gmail.com>
2022-12-04 20:15:39 -0300

    Desenvolvimento da interface

--- mainInterface.py
+++ mainInterface.py
@@ -1,6 +1,9 @@
 import sys
 from interface import *
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import QThread
+from sensor_bme280_term_10K import main
 
 
 class InterfaceEstacao(QMainWindow, Ui_MainWindow):
