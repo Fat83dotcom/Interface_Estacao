79dde53efc3f50ceee88afc8ec4a662b8f1bd1ec
fat83dotcom <fernando.quimic@gmail.com>
2023-01-01 16:01:16 -0300

    Reorganização de código

--- manipuladoresArquivos.py
+++ manipuladoresArquivos.py
@@ -7,21 +7,24 @@ mesma pasta que o executável Python
 def meu_email() -> list[str]:
     with open('.EMAIL_USER_DATA.txt', 'r') as file:
         file.seek(0)
-        email_user = [conteudo for e, conteudo in enumerate(file.readlines()) if e == 0 and conteudo != '\n']
+        email_user = [conteudo for e, conteudo in enumerate(file.readlines())
+                      if e == 0 and conteudo != '\n']
         return email_user
 
 
 def minha_senha() -> list[str]:
     with open('.PASSWORD_USER_DATA.txt', 'r') as file:
         file.seek(0)
-        senha_user = [conteudo for e, conteudo in enumerate(file.readlines()) if e == 0 and conteudo != '\n']
+        senha_user = [conteudo for e, conteudo in enumerate(file.readlines())
+                      if e == 0 and conteudo != '\n']
         return senha_user
 
 
 def my_recipients() -> list[str]:
     with open('.RECIPIENTS_USER_DATA.txt', 'r') as file:
         file.seek(0)
-        emails = [conteudo.strip() for conteudo in file.readlines() if conteudo != '\n']
+        emails = [conteudo.strip() for conteudo in file.readlines()
+                  if conteudo != '\n']
         return emails
 
 
