8e2bd52a90846b79a96a85eccdc52219d7b7b44a
fat83dotcom <fernando.quimic@gmail.com>
2023-08-08 22:52:23 -0300

    Criação de novas classes que cuidam de partes especificas - Diminuindo o acoplamento e redistribuindo responsabilidades.

--- DataGetter/DataGetter.py
+++ DataGetter/DataGetter.py
@@ -168,3 +168,139 @@ class ConexaoUSB():
 
     def desconectarPortaUSB(self) -> None:
         self.conexaoArduino.close()
+
+
+class InterfaceUtils:
+    def __init__(
+        self, saidaData: Signal, saidaDadosLCD: Signal,
+        barraProgresso: Signal, mostradorTempoRestante: Signal,
+        saidaInfoInicio: Signal, finalizar: Signal
+    ) -> None:
+        self.saidaData = saidaData
+        self.saidaDadosLCD = saidaDadosLCD
+        self.barraProgresso = barraProgresso
+        self.mostradorTempoRestante = mostradorTempoRestante
+        self.saidaInfoInicio = saidaInfoInicio
+        self.finalizar = finalizar
+
+    def porcentagem(self, totalVoltas: int, voltaAtual: int) -> int:
+        porcentagem: float = voltaAtual * 100 / totalVoltas
+        return int(porcentagem)
+
+    def enviarDadosTempoRealParaLCD(self, dados: dict) -> None:
+        dadosMostradorLcd: list = []
+        dadosMostradorLcd.append(dados["u"])
+        dadosMostradorLcd.append(dados["p"])
+        dadosMostradorLcd.append(dados["1"])
+        dadosMostradorLcd.append(dados["2"])
+        self.saidaData.emit(dataInstantanea())
+        self.saidaDadosLCD.emit(dadosMostradorLcd)
+
+    def atualizarBarraProgresso(self, tempoTotal, tempoCorrente) -> None:
+        percentTempoCorrido: int = self.porcentagem(tempoTotal, tempoCorrente)
+        self.barraProgresso.emit(percentTempoCorrido)
+
+    def atualizarTempoRestante(self, tempoTotal, tempoCorrente) -> None:
+        self.mostradorTempoRestante.emit((tempoTotal - tempoCorrente))
+
+    def atualizarFinalizacao(self) -> None:
+        self.saidaInfoInicio.emit('Programa Parado !!!')
+        self.barraProgresso.emit(0)
+        self.finalizar.emit()
+
+
+class FileUtils:
+    def __init__(self, saidaInfoInicio: Signal, ) -> None:
+        self.saidaInfoInicio = saidaInfoInicio
+
+    def registradorDadosArquivo(self, dados: dict) -> None:
+        with open(dataDoArquivo(), 'a+', newline='', encoding='utf-8') as log:
+            try:
+                if float(dados['u']) and float(dados['p']) and \
+                     float(dados['1']) and float(dados['2']) != 0:
+                    w = csv.writer(log)
+                    w.writerow(
+                        [
+                            dados['dt'], dados['u'],
+                            dados['p'], dados['1'],
+                            dados['2']
+                        ]
+                    )
+            except (ValueError, Exception) as e:
+                self.saidaInfoInicio.emit(
+                    f''' ATENÇÃO: Erro ao registrar dados no arquivo !!!
+                         ERRO: {e.__class__.__name__} -> {e}'''
+                )
+
+
+class DBUtils:
+    def __init__(self, dadosBD: dict, executor: ThreadPoolExecutor) -> None:
+        self.dadosBD = dadosBD
+        self.executor = executor
+        self.dB = OperationDataBase(self.dadosBD)
+        self.dDH = DadoHorario(self.dB)
+        self.dGT = GerenciadorTabelas(self.dB)
+
+    def inicializadorTabelasHorarias(self) -> None:
+        try:
+            tableName = self.dGT.nameTableGenerator()
+            self.dGT.execInsertTable(
+                (tableName,),
+                table='gerenciador_tabelas_horarias',
+                collumn=('data_tabela', )
+            )
+            fKey: int = self.dGT.getForeignKey()
+            self.executor.submit(
+                self.dDH.execCreateTable,
+                fKey, tableName=tableName, schema='tabelas_horarias'
+            )
+        except Exception:
+            ...
+
+    def verificaHorarioCriacaoTabelaHoraria(self) -> None:
+        now = datetime.now()
+        if now.hour == 0 and now.minute == 0 and now.second == 0:
+            self.inicializadorTabelasHorarias()
+
+    def insereBancoDados(self, dadosCarregadosArduino: dict) -> None:
+        tableName = self.dGT.nameTableGenerator()
+        self.executor.submit(
+            self.dDH.execInsertTable,
+            dadosCarregadosArduino,
+            table=tableName,
+            collumn=(
+                'data_hora', 'umidade', 'pressao', 'temp_int', 'temp_ext'
+            ),
+            schema='tabelas_horarias'
+        )
+
+
+class ArduinoUtils:
+    def __init__(self, portaArduino: Serial,) -> None:
+        self.arduino = portaArduino
+
+    def carregadorDados(self) -> list:
+        cargaDadosArduino: list = []
+        self.arduino.reset_input_buffer()
+        while len(cargaDadosArduino) < 4:
+            self.arduino.write('u'.encode('utf-8'))
+            sleep(0.1)
+            if self.arduino.in_waiting:
+                dado = self.arduino.readline().decode('utf-8')
+                cargaDadosArduino.append(dado.strip())
+            self.arduino.write('p'.encode('utf-8'))
+            sleep(0.1)
+            if self.arduino.in_waiting:
+                dado = self.arduino.readline().decode('utf-8')
+                cargaDadosArduino.append(dado.strip())
+            self.arduino.write('1'.encode('utf-8'))
+            sleep(0.1)
+            if self.arduino.in_waiting:
+                dado = self.arduino.readline().decode('utf-8')
+                cargaDadosArduino.append(dado.strip())
+            self.arduino.write('2'.encode('utf-8'))
+            sleep(0.1)
+            if self.arduino.in_waiting:
+                dado = self.arduino.readline().decode('utf-8')
+                cargaDadosArduino.append(dado.strip())
+        return cargaDadosArduino
